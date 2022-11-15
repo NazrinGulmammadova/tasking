9bcf12a528641ec7668d8b9b42b63483cfa2e5bd
NazrinGulmammadova <6jauppt@code.edu.az>
2022-11-15 21:54:24 +0400

    created cs file

---
+++ tasking.cs
@@ -0,0 +1,13 @@
+﻿int arrLength = int.Parse(Console.ReadLine());
+int[] arr = new int[arrLength];
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine("Enter array indexes:");
+    arr[i] = int.Parse(Console.ReadLine());
+    if (arr[i] % 2 == 0)
+    {
+        sum = arr[i] + sum;
+    }
+}
+Console.WriteLine(sum);
